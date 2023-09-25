b112fca02d3da520743f851e9773fc029072d32a
Eduardo Morais <84351592+EduardoVGVM@users.noreply.github.com>
2023-09-25 20:18:33 -0300

    Correção Entidades

--- src/main/java/CESAR/NExT/ParecerTecnicoAPI/entities/Cliente.java
+++ src/main/java/CESAR/NExT/ParecerTecnicoAPI/entities/Cliente.java
@@ -1,5 +1,7 @@
 package CESAR.NExT.ParecerTecnicoAPI.entities;
 
+import java.util.List;
+
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
 import jakarta.persistence.Column;
@@ -7,8 +9,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -20,7 +21,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Cliente {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
